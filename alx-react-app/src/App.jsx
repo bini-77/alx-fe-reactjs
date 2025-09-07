77da7baf35fddcf435ed2aecdfadde92065033b4
bini-77 <bekeleb921@gmail,com>
2025-09-07 22:18:53 +0300

    app.jsx commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 
 
+WelcomeMessage
 import WelcomeMessage from './components/WelcomeMessage
-
-
+<WelcomeMessage /> 
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
