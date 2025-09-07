24abfd51015290e34b44b645691d4668bc049b32
bini-77 <bekeleb921@gmail,com>
2025-09-07 22:09:25 +0300

    intial commmit

--- alx-react-app/src/WelcomeMessage.jsx
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -9,3 +9,14 @@ function WelcomeMessage() {
 }
 
 export default WelcomeMessage;
+
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
